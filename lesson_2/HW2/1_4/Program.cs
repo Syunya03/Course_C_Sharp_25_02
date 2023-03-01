36800d0f2c5205881ce667624653ba84bb53beea
Ксения <125123933+Syunya03@users.noreply.github.com>
2023-03-01 18:58:13 +0300

    1_4

---
+++ lesson_2/HW2/1_4/Program.cs
@@ -0,0 +1,50 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+ и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+
+
+string s_a = Console.ReadLine()!;
+
+if (s_a == "1")
+{
+    Console.WriteLine("<Будний день>");
+}
+else if (s_a == "2")
+{
+    Console.WriteLine("Будний день");
+}
+
+else if (s_a == "3")
+{
+    Console.WriteLine("Будний день");
+}
+
+else if (s_a == "4")
+{
+    Console.WriteLine("Будний день");
+}
+
+else if (s_a == "5")
+{
+    Console.WriteLine("Будний день");
+}
+
+else if (s_a == "6")
+{
+    Console.WriteLine("Выходной");
+}
+
+else if (s_a == "7")
+{
+    Console.WriteLine("Выходной");
+}
+
+else
+{
+    Console.WriteLine("Не день недели");
+}
