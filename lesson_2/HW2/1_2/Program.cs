f2187e9aab532deacf2f1544f3c8e38d48404f8d
Ксения <125123933+Syunya03@users.noreply.github.com>
2023-03-03 22:36:16 +0300

    1_3

---
+++ lesson_2/HW2/1_2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 1: Напишите программу, которая выводит случайное трёхзначное число 
+и удаляет вторую цифру из этого числа
+*/
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    int resulut1 = num % 100;
+    int resulut2 = num / 100;
+    if (resulut1 > resulut2)
+    {
+        return resulut1;
+    }
+    else
+    {
+        return resulut2;
+    }
+}
+
+int itog = TakeNum(new Random().Next(100, 1000));
+Console.WriteLine(itog);
