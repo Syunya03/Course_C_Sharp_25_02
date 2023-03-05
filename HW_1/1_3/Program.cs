134338e6d39fc7beb7e2672337c725cc9b665e9f
Ксения <125123933+Syunya03@users.noreply.github.com>
2023-03-05 19:17:14 +0300

    3_0

--- HW_1/1_3/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿/* Задача 6:
-Напишите программу, которая на вход принимает число и выдает, является ли число чётным
-4 -> да, -3 -> нет, 7 -> нет.
-*/
-
-int a = int.Parse(Console.ReadLine()!);
-
-if (a % 2 == 0) Console.WriteLine("Yes");
-else Console.WriteLine("No");
