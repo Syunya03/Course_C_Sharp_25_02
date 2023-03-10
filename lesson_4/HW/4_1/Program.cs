66dc85280021012198eb1100dba547551864496a
Ксения <125123933+Syunya03@users.noreply.github.com>
2023-03-10 14:53:16 +0300

    4_1hw

---
+++ lesson_4/HW/4_1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A 
+в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+
+int Degree(int num_A, int num_B)
+{
+  int res = 1;
+  for(int i=1; i <= num_B; i++)
+  {
+    res = res * num_A;
+  }
+  
+    return res;
+}
+
+  Console.Write("Введите число: ");
+  int num_A = int.Parse(Console.ReadLine()!);
+  Console.Write("Введите cтепень числа: ");
+  int num_B = int.Parse(Console.ReadLine()!);
+
+  int value = Degree(num_A, num_B);
+  Console.WriteLine("Ответ: " + value);
+
