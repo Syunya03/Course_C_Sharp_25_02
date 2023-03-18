7374bba7ccc1414a1206b0e66f460223da34dec8
Ксения <125123933+Syunya03@users.noreply.github.com>
2023-03-18 20:32:26 +0300

    6_3hw

--- Lesson_6/HW/6_3/Program.cs
+++ Lesson_6/HW/6_3/Program.cs
@@ -4,17 +4,27 @@
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
 
+void Straight_line(double b1, double k1,double b2, double k2)
+{if(b1==b2 && k1==k2)
+Console.WriteLine("Прямые совпадают");
+else if (k1==k2)
+Console.WriteLine("Прямые параллельны");
+else
+   {double line_X =(b2-b1)/(k1-k2);
+     double line_Y= (k1*(b2-b1))/(k1-k2)+b1;
+     Console.WriteLine($"line_X= {line_X}");
+     Console.Write($"line_Y= {line_Y}");
+   }
+return;
+}
+Console.WriteLine("Введите b1: ");
+double b1= double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k1:");
+double k1= double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите b2:");
+double b2= double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k2:");
+double k2= double.Parse(Console.ReadLine()!);
+Straight_line(b1,k1,b2,k2);
 
-double x = -(b1 - b2) / (k1 - k2);
-double y = k1 * x + b1;
-Console.Write($" Точка пересечения двух прямых: [{x},{y}]");
-
-Console.WriteLine("Введите переменную b1: ");
-double b1 = double.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите переменную k1: ");
-double k1 = double.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите переменную b2: ");
-double b2 = double.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите переменную k2: ");
-double k2 = double.Parse(Console.ReadLine()!);
 
