1df63028054f8648aa64a47bc38f4c627cde986b
Ксения <125123933+Syunya03@users.noreply.github.com>
2023-03-25 00:07:58 +0300

    8_3HWchange

--- Lesson_8/HW/8_3/Program.cs
+++ Lesson_8/HW/8_3/Program.cs
@@ -1,12 +1,11 @@
-﻿/*Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
- и возвращает значение этого элемента или же указание, что такого элемента нет.
-Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-17 -> такого числа в массиве нет
+﻿/*Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
 */
-
 
 void Print(int[,] arr)
 {
@@ -22,6 +21,20 @@ void Print(int[,] arr)
     Console.WriteLine();
 }
 
+void Print_2(int[,] arr_2)
+{
+    int row = arr_2.GetLength(0);
+    int column = arr_2.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr_2[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
 int[,] MassNums(int row, int column, int from, int to)
 {
     int[,] arr = new int[row, column];
@@ -32,33 +45,59 @@ int[,] MassNums(int row, int column, int from, int to)
 
     return arr;
 }
-void Print_2(int[,] arr, int row, int column)
+
+void Result(int[,] arr, int[,] arr_2)
 {
-    if (row< 0 | row> arr.GetLength(0) - 1 |column < 0 | column> arr.GetLength(1) - 1)
+    int[,] result_arr = new int[arr.GetLength(0), arr_2.GetLength(1)];
+
     {
-        Console.WriteLine("Элемент не существует");
+        if (arr.GetLength(0) != arr_2.GetLength(1))
+        {
+            Console.WriteLine(" Нельзя перемножить ");
+            return;
+        }
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr_2.GetLength(1); j++)
+            {
+                result_arr[i, j] = 0;
+                for (int k = 0; k < arr.GetLength(1); k++)
+                {
+                    result_arr[i, j] += arr[i, k] * arr_2[k, j];
+                }
+                
+                Console.WriteLine($" {result_arr[i,j]}");
+               
+            }
+        }
     }
-    else
-    {
-        Console.WriteLine("Значение элемента массива = {0}", arr[row-1, column-1]);
-    }
+    
 }
 
 
-
-Console.Write("Введите количество строк: ");
+Console.Write("Введите количество строк 1го массива: ");
 int num_row = int.Parse(Console.ReadLine()!);
-Console.Write("Введите количество столбцов: ");
+Console.Write("Введите количество столбцов1го массива: ");
 int num_column = int.Parse(Console.ReadLine()!);
-Console.Write("Введите начало массива: ");
+Console.Write("Введите начало 1го массива: ");
 int start = int.Parse(Console.ReadLine()!);
-Console.Write("Введите окончание массива: ");
+Console.Write("Введите окончание 1го массива: ");
 int stop = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+Console.Write("Введите количество строк 2го массива: ");
+int num_row_2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов 2го массива: ");
+int num_column_2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите начало 2го массива: ");
+int start_2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите окончание 2го массива: ");
+int stop_2 = int.Parse(Console.ReadLine()!);
 
 int[,] mass = MassNums(num_row, num_column, start, stop);
+int[,] mass_2 = MassNums(num_row, num_column, start, stop);
 Print(mass);
-Console.Write("Введите строку: ");
-int row = int.Parse(Console.ReadLine()!);
-Console.Write("Введите столбец: ");
-int column = int.Parse(Console.ReadLine()!);
-Print_2(mass, row, column);
+Print_2(mass_2);
+Result(mass,mass_2);
+
+
